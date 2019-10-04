dda6ae10aabeee67ad348580541cae0d1b76d07f
Arzen Edillo <aedillo@Arzens-MacBook-Pro.local>
2019-10-04 08:30:06 -0400

    First commit

---
+++ main/BankAccount.py
@@ -0,0 +1,13 @@
+class BackAccount:
+    def __init__(self):
+        _bankaccountNumber = 0
+        _initialBalance = 0
+        _interestRate = 0.3
+        _accountHolderName = ''
+        _monthlyDeposit = 0
+        _monthlyWithdrawal = 0
+        bankInformationSubmitted = False
+
+    def bankProgram(self):
+        #while(bankInformationIn):
+    
