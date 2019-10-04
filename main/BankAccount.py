45f23cb59fc07b4dbcaa8875c7c5451dbf5e0fd5
Arzen Edillo <aedillo@Arzens-MacBook-Pro.local>
2019-10-04 08:43:16 -0400

    Created the object and the main program for this assignment

--- main/BankAccount.py
+++ main/BankAccount.py
@@ -1,4 +1,4 @@
-class BackAccount:
+class BankAccount:
     def __init__(self):
         _bankaccountNumber = 0
         _initialBalance = 0
@@ -6,8 +6,26 @@ class BackAccount:
         _accountHolderName = ''
         _monthlyDeposit = 0
         _monthlyWithdrawal = 0
-        bankInformationSubmitted = False
 
     def bankProgram(self):
-        #while(bankInformationIn):
+        display = 'Welcome to Programming Principles Bank'
+        print(display)
+        bankInformationSubmitted = False
+        try:
+            while(bankInformationSubmitted != True):
+                inputAccountHolderName = input('Please enter the name of hte account holder: ')
+                self._accountHolderName = inputAccountHolderName  
+                inputAccountNumber = input('Please enter the account number: ')
+                self._bankAccountNumber = inputAccountNumber 
+                inputAnnualInterestRate = input('Please enter the annual interest rate percentage: ')
+                self._interestRate = inputAnnualInterestRate  
+                inputInitialBalance = input('Please enter the initial balance: ')
+                self._initialBalance = inputInitialBalance  
+                inputMonthlyDeposit = input('Please enter the monthly deposit: ')
+                self._MonthlyDeposit = inputMonthlyDeposit  
+                inputMonthlyWithdrawal = input('Please enter the monthly withdrawal: ')
+                self._monthlyWithdrawal = inputMonthlyWithdrawal  
+                bankInformationSubmitted = True
+        except():
+            print('Something wrong has happened here...')
     
---
+++ main/Program.py
@@ -0,0 +1,5 @@
+import BankAccount
+
+#Create a BankAccount object then call the function created inside of the object
+BankAccount1 = BankAccount()
+
