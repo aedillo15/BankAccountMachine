1fe3c0c4966c4219548d9722f0dc7045814da979
Arzen Edillo <aedillo@Arzens-MacBook-Pro.local>
2019-10-05 09:02:38 -0400

    Made changes to the input prints to match of the ones of the assignment

--- main/Test.py
+++ main/Test.py
@@ -1,20 +1,3 @@
 import BankStatement
-inputAccountHolderName = ''
-splitName = ''
-Name = ''
-#Input, Assignment, Validation for the _accountHolderName
-while(Name.isalpha() != True): #Ask user until the input is a valid name (isalpha() and if digit ask them again)
-    try:
-        inputAccountHolderName = input('Please enter the name of the account holder: ')
-        if inputAccountHolderName.isdigit(): #Validation
-            print('Please enter a valid name')
-            continue   
-        splitName = inputAccountHolderName.split() #Input
-        firstName = splitName[0]
-        Name = firstName
-        if(len(splitName) == 2):
-            lastName = splitName[1]
-            Name += lastName
-    
-    except ValueError:
-        print('Please enter a valid name')
+#Tested: Input Bank Holder Name, Input Account Number
+#Testing: Input Annual Interest Rate, Initial Balance, Monthly Deposit, Monthly Withrawal
