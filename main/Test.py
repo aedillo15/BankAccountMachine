f769a1c33f5818d42b03c582348750b47013a183
Arzen Edillo <aedillo@Arzens-MacBook-Pro.local>
2019-10-08 13:25:21 -0400

    About to test the inputMonthlyWithdrawal

--- main/Test.py
+++ main/Test.py
@@ -2,12 +2,12 @@ import BankStatement
 #Tested: Input Bank Holder Name, Input Account Number
 
 #Testing: Input Annual Interest Rate, Initial Balance, Monthly Deposit, Monthly Withrawal
-inputMonthlyDeposit = ''
-while(type(inputMonthlyDeposit) != float): # While loop until the inputInitialBalance is equal to a float
-    inputMonthlyDeposit = input('Please enter the monthly deposit [> 100]: ')
-    if inputMonthlyDeposit.isalpha() == True:#Validate that inputIntialBalance is not alphabetical
+inputMonthlyWithdrawal = ''
+while(type(inputMonthlyWithdrawal) != float): # While loop until the inputInitialBalance is equal to a float
+    inputMonthlyWithdrawal = input('Please enter the monthly withdrawal [< 1000]: ')
+    if inputMonthlyWithdrawal.isalpha() == True:#Validate that inputIntialBalance is not alphabetical
         print('Please enter a valid balance that is numeric.')  
-    if float(inputMonthlyDeposit) < 100:  #Validate that inputInitialBalance less than or equal to 0
+    if float(inputMonthlyWithdrawal) < 100:  #Validate that inputInitialBalance less than or equal to 0
         print('Please enter a valid balance greater than 100.')
-    if float(inputMonthlyDeposit) >= 100:
-        inputMonthlyDeposit = float(inputMonthlyDeposit) 
+    if float(inputMonthlyWithdrawal) >= 100:
+        inputMonthlyWithdrawal = float(inputMonthlyWithdrawal) 
